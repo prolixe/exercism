0cb1b2b48ee76980c2268b7a9be72c79cfc3bdfb
Lucas Joyal <lucasjoyal@gmail.com>
2017-07-16 20:00:04 -0400

    tree-building first iteration. Naive and ugly but the fastest I found

---
+++ go/tree-building/tree_building.go
@@ -0,0 +1,122 @@
+package tree
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+const testVersion = 4
+
+type Record struct {
+	ID, Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+type NodeSlice []*Node
+
+func (n NodeSlice) Len() int {
+	return len(n)
+}
+
+func (n NodeSlice) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+func (n NodeSlice) Less(i, j int) bool { return n[i].ID < n[j].ID }
+
+type Stack struct {
+	nodes []*Node
+	count int
+}
+
+func (s *Stack) Push(n *Node) {
+	s.nodes = append(s.nodes[:s.count], n)
+	s.count++
+}
+
+func (s *Stack) Pop() *Node {
+	if s.count == 0 {
+		return nil
+	}
+	s.count--
+	return s.nodes[s.count]
+}
+
+type Mismatch struct{}
+
+func (m Mismatch) Error() string {
+	return "c"
+}
+
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	// Validate the records
+	for _, r := range records {
+		if r.ID <= r.Parent && r.ID != r.Parent {
+			return nil, errors.New("ID equal or greater than Parent, while not being root!")
+		}
+	}
+
+	// Find root
+	var root Node
+	var rootFound bool
+	processedRecord := make(map[int]bool)
+	for _, r := range records {
+		if r.ID == r.Parent {
+			root = Node{ID: r.ID}
+			rootFound = true
+			break
+		}
+	}
+	if !rootFound {
+		return nil, errors.New("root not found, invalid records")
+	}
+	nodesStack := Stack{}
+	nodesStack.Push(&root)
+	processedRecord[root.ID] = true
+	nodeSize := 1
+	for nodeSize < len(records) {
+		currentNode := nodesStack.Pop()
+		if currentNode == nil {
+			break
+		}
+		nodeSize += findChildren(records, currentNode)
+		for _, c := range currentNode.Children {
+			if processedRecord[c.ID] {
+				fmt.Println(c)
+				return nil, errors.New("Already processed this record id. Either cycle or duplicates")
+			}
+			nodesStack.Push(c)
+			processedRecord[c.ID] = true
+		}
+	}
+
+	// Testing for continuous ids
+	for i := range records {
+		if !processedRecord[i] {
+			return nil, errors.New("Non-countinous record id!")
+		}
+	}
+
+	return &root, nil
+
+}
+
+func findChildren(records []Record, node *Node) (foundCount int) {
+
+	// Linear Search for all children of node in the record
+	for _, r := range records {
+		if r.Parent == node.ID && r.ID != r.Parent {
+			node.Children = append(node.Children, &Node{ID: r.ID})
+			foundCount++
+		}
+	}
+	sort.Sort(NodeSlice(node.Children))
+	return
+
+}
