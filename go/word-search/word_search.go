92affb588d69930e3a252a200de3a3a3598ad3d3
Lucas Joyal <lucasjoyal@gmail.com>
2017-09-02 08:31:00 -0400

    Rework the algorithm using a Direction struct. No longer cheat for the last test case, implementation now works completely

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -1,7 +1,5 @@
 package wordsearch
 
-import "strings"
-
 const testVersion = 3
 
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
@@ -13,93 +11,67 @@ func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 		}
 
 	}
-	positions["go"] = [2][2]int{{8, 4}, {7, 3}}
 	return positions, nil
 }
 
-func reverse(word string) string {
-	var rw string
-	for i := len(word) - 1; i >= 0; i-- {
-		rw += string(word[i])
-	}
-	return rw
+type Direction struct {
+	x, y int
 }
 
-func Transpose(input []string) []string {
-
-	var maxLength int
-	for _, s := range input {
-		if len(s) > maxLength {
-			maxLength = len(s)
-		}
-	}
-
-	// Create a 2D byte array full of spaces
-	byte2DArray := make([][]byte, maxLength)
-	for i := range byte2DArray {
-		byte2DArray[i] = make([]byte, len(input))
-		for j := range byte2DArray[i] {
-			byte2DArray[i][j] = ' '
-		}
-	}
-
-	// Fill it with the input transposed
-	for i, s := range input {
-		for j, r := range s {
-			byte2DArray[j][i] = byte(r)
-		}
-	}
-
-	//Reconvert it into an slice of strings and strip the extra whitespace
-	output := make([]string, maxLength)
-	for i, bArray := range byte2DArray {
-		output[i] = string(bArray)
-		if i == len(byte2DArray)-1 {
-			output[i] = strings.TrimRight(output[i], " ")
-		}
-	}
-
-	return output
+var directions = [8]Direction{
+	{-1, -1},
+	{-1, 0},
+	{-1, 1},
+	{0, 1},
+	{0, -1},
+	{1, -1},
+	{1, 0},
+	{1, 1},
 }
 
 func solve(word string, puzzle []string) (pos [2][2]int, found bool) {
 
-	wl := len(word)
-	// Check all position horizontally
-	for y, line := range puzzle {
-
-		for x := 0; x+wl <= len(line); x++ {
-			if word == line[x:x+wl] {
-				pos = [2][2]int{{x, y}, {x + wl - 1, y}}
-				found = true
-				return
-			}
-			if reverse(word) == line[x:x+wl] {
-				pos = [2][2]int{{x + wl - 1, y}, {x, y}}
-				found = true
-				return
-			}
-
-		}
-	}
-
-	// Check all position vertically
-	transposedPuzzle := Transpose(puzzle)
-	for x, line := range transposedPuzzle {
-		for y := 0; y+wl <= len(line); y++ {
-			if word == line[y:y+wl] {
-				pos = [2][2]int{{x, y}, {x, y + wl - 1}}
-				found = true
-				return
-			}
-			if reverse(word) == line[y:y+wl] {
-				pos = [2][2]int{{y + wl - 1, x}, {x, y}}
-				found = true
-				return
+	for y, row := range puzzle {
+		for x, c := range row {
+			if c == rune(word[0]) {
+				for _, dir := range directions {
+					if posLast, ok := match(word, x, y, dir, puzzle); ok {
+						pos = [2][2]int{{x, y}, posLast}
+						found = ok
+						return
+					}
+				}
 			}
 
 		}
 	}
 
 	return
+}
+
+func match(word string, x, y int, dir Direction, puzzle []string) (posLast [2]int, found bool) {
+	for i, c := range word {
+		r, ok := puzzleAt(puzzle, x+i*dir.x, y+i*dir.y)
+		if !ok {
+			return
+		}
+		if r != byte(c) {
+			return
+		}
+	}
+	posLast = [2]int{x + (len(word)-1)*dir.x, y + (len(word)-1)*dir.y}
+	found = true
+	return
+
+}
+
+func puzzleAt(puzzle []string, x, y int) (byte, bool) {
+	if y < 0 || len(puzzle) <= y {
+		return ' ', false
+	}
+	row := puzzle[y]
+	if x < 0 || len(row) <= x {
+		return ' ', false
+	}
+	return puzzle[y][x], true
 }
