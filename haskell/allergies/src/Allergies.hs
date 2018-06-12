062d748d443f09b6505a9a280a3881155332b64e
Lucas Joyal <lucasjoyal@gmail.com>
2018-06-11 20:19:21 -0400

    Refactor solution after seeing all approach to this problem

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -15,19 +15,10 @@ data Allergen
   | Chocolate
   | Pollen
   | Cats
-  deriving (Eq, Enum)
+  deriving (Eq, Enum, Show)
 
 allergies :: Int -> [Allergen]
-allergies score
-  | popCount (score .&. bit 0) == 1 = Eggs : allergies (score - bit 0)
-  | popCount (score .&. bit 1) == 1 = Peanuts : allergies (score - bit 1)
-  | popCount (score .&. bit 2) == 1 = Shellfish : allergies (score - bit 2)
-  | popCount (score .&. bit 3) == 1 = Strawberries : allergies (score - bit 3)
-  | popCount (score .&. bit 4) == 1 = Tomatoes : allergies (score - bit 4)
-  | popCount (score .&. bit 5) == 1 = Chocolate : allergies (score - bit 5)
-  | popCount (score .&. bit 6) == 1 = Pollen : allergies (score - bit 6)
-  | popCount (score .&. bit 7) == 1 = Cats : allergies (score - bit 7)
-  | otherwise = []
+allergies score = filter (`isAllergicTo` score) [Eggs ..]
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = allergen `elem` allergies score
+isAllergicTo allergen score = testBit score (fromEnum allergen)
