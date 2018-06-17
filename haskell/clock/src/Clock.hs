c1e108a5c0a32c9144a86180c8544d33a133f59c
Lucas Joyal <lucasjoyal@gmail.com>
2018-06-16 21:47:27 -0400

    Refactor pascal triangle solution to be clearer

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -8,14 +8,31 @@ module Clock
 
 import           Text.Printf
 
-data Clock = Clock
-  { clockHour :: Int
-  , clockMin  :: Int
-  } deriving (Eq, Show)
+newtype Clock =
+  Clock Int
+  deriving (Show)
+
+clockHour :: Clock -> Int
+clockHour (Clock c) = (c `div` 60) `mod` 24
+
+clockMin :: Clock -> Int
+clockMin (Clock c) = c `mod` 60
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin = Clock
+fromHourMin hour min = Clock ((hour * 60) + min)
 
 toString :: Clock -> String
 toString clock =
   printf "%02d" (clockHour clock) ++ ":" ++ printf "%02d" (clockMin clock)
+
+instance Num Clock where
+  (+) (Clock c1) (Clock c2) = Clock (c1 + c2)
+  (-) (Clock c1) (Clock c2) = Clock (c1 - c2)
+  (*) (Clock c1) (Clock c2) = Clock (c1 * c2)
+  negate (Clock c) = Clock (abs (c - 24 * 60))
+  abs (Clock c) = Clock (abs c)
+  fromInteger i = Clock (fromIntegral i)
+  signum (Clock c) = Clock (signum c)
+
+instance Eq Clock where
+  (==) c1 c2 = toString c1 == toString c2
--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -6,10 +6,11 @@ rows :: Int -> [[Integer]]
 rows 0 = []
 rows x = map row [1 .. x]
 
-row :: Int -> [Integer]
-row 0 = []
-row x = 1 : zipWith (+) (row (x - 1)) shiftedRow
-  where
-    shiftedRow = drop 1 (row (x - 1)) ++ [0 ..]
 -- We can visualize each row as the sum of the previous row
--- with itself, shifted by 1.
+-- with itself, shifted by 1 and zero padded
+row :: Int -> [Integer]
+row 1 = [1]
+row x = take x $ zipWith (+) zeroPaddedRow shiftedRow
+  where
+    shiftedRow = 0 : zeroPaddedRow
+    zeroPaddedRow = row (x - 1) ++ [0 ..]
