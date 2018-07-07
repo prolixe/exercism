fef93f3df12906cedad186e7d9bbbe9fcca6bc3c
Lucas Joyal <lucasjoyal@gmail.com>
2018-07-07 09:28:30 -0400

    Add solution for queen-attack problem

---
+++ haskell/queen-attack/src/Queens.hs
@@ -0,0 +1,43 @@
+module Queens
+  ( boardString
+  , canAttack
+  ) where
+
+import           Data.List
+import           Data.Maybe
+
+boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
+boardString white black
+  | isNothing white && isNothing black = emptyBoard
+  | isNothing white = placeInRank 'B' (fromJust black) emptyBoard
+  | isNothing black = placeInRank 'W' (fromJust white) emptyBoard
+  | otherwise =
+    placeInRank 'B' (fromJust black) $
+    placeInRank 'W' (fromJust white) emptyBoard
+  where
+    emptyBoard = unlines $ replicate 8 $ intersperse ' ' $ replicate 8 '_'
+    placeInRank c queen board =
+      unlines $
+      map
+        (\(i, r) ->
+           if i == fst queen
+             then placeQueen c queen
+             else r) $
+      zip [0 ..] (lines board)
+    placeQueen c queen =
+      intersperse ' ' $
+      map
+        (\i ->
+           if snd queen == i
+             then c
+             else '_')
+        [0 .. 7]
+
+canAttack :: (Int, Int) -> (Int, Int) -> Bool
+canAttack queenA queenB =
+  rankA == rankB || fileA == fileB || abs (rankA - rankB) == abs (fileA - fileB)
+  where
+    rankA = fst queenA
+    rankB = fst queenB
+    fileA = snd queenA
+    fileB = snd queenB
